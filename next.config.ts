566d5f6e0d5f2ca6c4124741ac0e2442748dd4b7
vdb3 <bogadovd@hotmail.com>
2024-11-07 18:31:10 -0300

    started

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
 
+
 export default nextConfig;
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/dv_repertorio_latest/' : '',
+  basePath: isProd ? '/dv_repertorio_latest' : '',
+};
+
